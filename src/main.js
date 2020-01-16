7cababf2c905cf6bf5b35e39252e9f854657d546
mating <474200880@qq.com>
2020-01-16 17:00:00 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,12 +4,26 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Element from 'element-ui'
+import i18n from '@/lang'
+
+import '../static/css/index.css' // 初始化基础样式
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Element, {
+    size: 'medium',
+    i18n: (key, value) => i18n.t(key, value)
+  })
+
+import dayjs from 'dayjs' //全局引用dayjs
+window.dayjs = dayjs;
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
